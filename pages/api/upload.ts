4d5de3659a92947c2d5cfc5e6a2b5d75486d09c1
test <72378859+CronoMail@users.noreply.github.com>
2025-02-13 06:00:48 +0000

    #$

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -113,8 +113,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               <p>Twitter</p>
             </div>` : ''}
           </div>
-
-          <!-- ========== IMAGE END ========== -->
+ <!-- ========== IMAGE END ========== -->
+         
 
           `;
 
